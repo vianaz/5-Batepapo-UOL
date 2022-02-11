edc3de11151baa235ed8cbefb259ec5e6ebc94d5
Gabriel Viana Boaes <viana_gab2001@hotmail.com>
2022-02-11 14:50:40 -0300

    Commit aleatorio

--- script/script.js
+++ script/script.js
@@ -1,37 +1,67 @@
 // Variaveis que guardam os HTML
 let nomeUsuario = "";
-let promisse = "";
+let ultimoUsuario;
+let ultimoUsuarioGuardado;
+let indexUltimoUsuario;
 
 function pedirNomeUsuario() {
-    nomeUsuario = prompt('Escolha um nome de usuário')
-    axios.post('https://mock-api.driven.com.br/api/v4/uol/participants', {
-        name: `${nomeUsuario}`
-    }).then(verficarNomeUsuario)
-}
-
-function verficarNomeUsuario(promisse) {
-    if (promisse.status === 200) {
-        axios.get('https://mock-api.driven.com.br/api/v4/uol/participants').then( (response) => {
-            avisoEntradaNaSala(response.data[0].name);
+    let erro = true;
+    while (erro) {
+        nomeUsuario = prompt('Escolha um nome de usuário');
+        // ultimoUsuarioGuardado = nomeUsuario;
+        axios.post('https://mock-api.driven.com.br/api/v4/uol/participants', {
+            name: `${nomeUsuario}`
+        }).then(() => {
+            verificarParticipates();
+            erro = false;
         });
-        setInterval(manterConexao, 5000);
-    } else{
-        pedirNomeUsuario();
     }
 }
-function avisoEntradaNaSala(nomePessoa) {
-    document.querySelector('main').innerHTML = `
-    <div class="entrou-sala texto">
-            <p><span>(09:21:45)</span><strong>${nomePessoa}</strong> entrou na sala...</p>
-        </div>
-`;
-}
-function manterConexao() {
-    axios.post('https://mock-api.driven.com.br/api/v4/uol/status', {
-        name: `${nomeUsuario}`
-    });
+
+function verificarNomeUsuario() {
+    verificarParticipates();
+    manterConexao();
 }
 
+function avisoEntradaNaSala(response) {
+    if (ultimoUsuarioGuardado.from === response.data[99].from && ultimoUsuarioGuardado.type === response.data[99].type && ultimoUsuarioGuardado.time === response.data[99].time) {
+        console.log(respose.data[99].from);
+        return;
+    }
+    const indexUltimoUsuario = response.data.findIndex( (element) => element.time === ultimoUsuarioGuardado.time);
+    console.log('Mudou Guardado:');
+    for (let index = indexUltimoUsuario; index < response.data.length; index++) {
+        console.log(response.data[index]);
+        if (response.data[index].type === "status") {
+            document.querySelector('main').innerHTML = document.querySelector('main').innerHTML + `
+            <div class="entrou-sala texto">
+                <p><span>${response.data[index].time}</span><strong>${response.data[index].from}</strong> ${response.data[index].text}</p>
+            </div>
+        `;
+        }
+    }
+    ultimoUsuarioGuardado = response.data[99];
+}
+
+    
+
+function verificarParticipates(){
+    axios.get('https://mock-api.driven.com.br/api/v4/uol/messages').then((response) => {
+        ultimoUsuarioGuardado = response.data[99];
+    }); 
+
+    setInterval(() => {
+        axios.get('https://mock-api.driven.com.br/api/v4/uol/messages').then(avisoEntradaNaSala);
+    }, 3000);
+}
+
+function manterConexao() {
+    setInterval(() => {
+        axios.post('https://mock-api.driven.com.br/api/v4/uol/status', {
+            name: `${nomeUsuario}`
+        });
+    }, 5000);
+}
 
 
 
